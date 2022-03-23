9b04a733e21cc07031e83a7ad664a208d1a7758a
alexanderJr <alexmarcjr21@gmail.com>
2022-03-23 16:31:39 -0300

    Configurando ambiente (erro no babel-webpack)

---
+++ dist/bundle.js
@@ -0,0 +1,32 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/index.jsx":
+/*!***********************!*\
+  !*** ./src/index.jsx ***!
+  \***********************/
+/***/ (() => {
+
+eval("throw new Error(\"Module build failed (from ./node_modules/babel-loader/lib/index.js):\\nSyntaxError: C:\\\\ignite-aulas\\\\reactjs\\\\github-explorer\\\\src\\\\index.jsx: Identifier 'React' has already been declared. (2:7)\\n\\n\\u001b[0m \\u001b[90m 1 |\\u001b[39m \\u001b[36mimport\\u001b[39m \\u001b[33mReact\\u001b[39m \\u001b[36mfrom\\u001b[39m \\u001b[32m\\\"react\\\"\\u001b[39m\\u001b[33m;\\u001b[39m\\u001b[0m\\n\\u001b[0m\\u001b[31m\\u001b[1m>\\u001b[22m\\u001b[39m\\u001b[90m 2 |\\u001b[39m \\u001b[36mimport\\u001b[39m \\u001b[33mReact\\u001b[39m \\u001b[36mfrom\\u001b[39m \\u001b[32m\\\"react-dom\\\"\\u001b[39m\\u001b[33m;\\u001b[39m\\u001b[0m\\n\\u001b[0m \\u001b[90m   |\\u001b[39m        \\u001b[31m\\u001b[1m^\\u001b[22m\\u001b[39m\\u001b[0m\\n\\u001b[0m \\u001b[90m 3 |\\u001b[39m \\u001b[36mimport\\u001b[39m { render } \\u001b[36mfrom\\u001b[39m \\u001b[32m\\\"react-dom\\\"\\u001b[39m\\u001b[33m;\\u001b[39m\\u001b[0m\\n\\u001b[0m \\u001b[90m 4 |\\u001b[39m \\u001b[36mimport\\u001b[39m { \\u001b[33mApp\\u001b[39m } \\u001b[36mfrom\\u001b[39m \\u001b[32m\\\"./App\\\"\\u001b[39m\\u001b[33m;\\u001b[39m\\u001b[0m\\n\\u001b[0m \\u001b[90m 5 |\\u001b[39m\\u001b[0m\\n    at instantiate (C:\\\\ignite-aulas\\\\reactjs\\\\github-explorer\\\\node_modules\\\\@babel\\\\parser\\\\lib\\\\index.js:72:32)\\n    at constructor (C:\\\\ignite-aulas\\\\reactjs\\\\github-explorer\\\\node_modules\\\\@babel\\\\parser\\\\lib\\\\index.js:358:12)\\n    at Parser.raise (C:\\\\ignite-aulas\\\\reactjs\\\\github-explorer\\\\node_modules\\\\@babel\\\\parser\\\\lib\\\\index.js:3334:19)\\n    at ScopeHandler.checkRedeclarationInScope (C:\\\\ignite-aulas\\\\reactjs\\\\github-explorer\\\\node_modules\\\\@babel\\\\parser\\\\lib\\\\index.js:3518:19)\\n    at ScopeHandler.declareName (C:\\\\ignite-aulas\\\\reactjs\\\\github-explorer\\\\node_modules\\\\@babel\\\\parser\\\\lib\\\\index.js:3484:12)\\n    at Parser.declareNameFromIdentifier (C:\\\\ignite-aulas\\\\reactjs\\\\github-explorer\\\\node_modules\\\\@babel\\\\parser\\\\lib\\\\index.js:12057:16)\\n    at Parser.checkIdentifier (C:\\\\ignite-aulas\\\\reactjs\\\\github-explorer\\\\node_modules\\\\@babel\\\\parser\\\\lib\\\\index.js:12052:12)\\n    at Parser.checkLVal (C:\\\\ignite-aulas\\\\reactjs\\\\github-explorer\\\\node_modules\\\\@babel\\\\parser\\\\lib\\\\index.js:11978:12)\\n    at Parser.finishImportSpecifier (C:\\\\ignite-aulas\\\\reactjs\\\\github-explorer\\\\node_modules\\\\@babel\\\\parser\\\\lib\\\\index.js:16182:10)\\n    at Parser.parseImportSpecifierLocal (C:\\\\ignite-aulas\\\\reactjs\\\\github-explorer\\\\node_modules\\\\@babel\\\\parser\\\\lib\\\\index.js:16178:31)\");\n\n//# sourceURL=webpack://github-explorer/./src/index.jsx?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module doesn't tell about it's top-level declarations so it can't be inlined
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/index.jsx"]();
+/******/ 	
+/******/ })()
+;
